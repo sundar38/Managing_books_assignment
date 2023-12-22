f244de2ef2c905ff08f394dd90be68bc903e1391
Penugonda <sundara.k.penugonda@accenture.com>
2023-12-22 15:47:52 +0530

    completed sorting, filtering

---
+++ src/Bookslist.js
@@ -0,0 +1,93 @@
+const bookslist=[
+    { 
+        "id": 1, "title": "The Official Raspberry Pi Handbook 2023", "author": "David Crookes", "publication_year": 1978,"genre": "Comedy",
+    },
+    {
+        "id": 2, "title": "Pay for Play", "author": "Larry Wayte", "publication_year": 1965, "genre": "Fiction",
+    },
+    {
+        "id": 3, "title": "Azure Bot Service Succinctly", "author": "Ed Freitas", "publication_year": 1995, "genre": "Thriller",
+    },
+    {
+        "id": 4, "title": "Introductory Algebra", "author": "Anne Gloag", "publication_year": 1987, "genre": "Comedy",
+    },
+    {
+        "id": 5, "title": "Managing Cloud Native Data on Kubernetes", "author": "Jeff Carpenter", "publication_year": 1979, "genre": "Novel",
+    },
+    {
+        "id": 6, "title": "An Introduction to C &amp; GUI Programming", "author": "Simon Long", "publication_year": 2005, "genre": "Comedy",
+    },
+    {
+        "id": "7", "title": "Contemporary Mathematics", "author": "Donna Kirk", "publication_year": 2008, "genre": "Poetry",
+    },
+    {
+        "id": "8", "title": "The Last Man Who Knew Everything", "author": "Andrew Robinson", "publication_year": 1990, "genre": "Comedy",
+    },
+    {
+        "id": "9", "title": "HackSpace Magazine: Issue 65", "author": "HackSpace Team", "publication_year": 2010, "genre": "Thriller",
+    },
+    {
+        "id": "10", "title": "Intro to Social Media", "author": "Cheryl Lawson", "publication_year": 2000, "genre": "Comedy",
+    },
+    {
+        "id": "11", "title": "HackSpace magazine: Issue 64", "author": "HackSpace Team", "publication_year": 1990, "genre": "Comedy",
+    },
+    {
+        "id": "12", "title": "Introduction to Mechanical Engineering Design", "author": "Jacqulyn A. Baughman", "publication_year": 2013, "genre": "Novel",
+    },
+    {
+        "id": "13", "title": "Svelte Succinctly", "author": "Ed Freitas", "publication_year": 1980, "genre": "Comedy",
+    },
+    {
+        "id": "14", "title": "Introduction to Systems Biology", "author": "Thomas Sauter", "publication_year": 1976, "genre": "Fiction",
+    },
+    {
+        "id": "15", "title": "Blockchain For Dummies", "author": "Manav Gupta", "publication_year": 1992, "genre": "Fiction",
+    },
+    {
+        "id": "16", "title": "ASP.NET Core APIs Succinctly", "author": "Dirk Strauss", "publication_year": 1978, "genre": "Drama",
+    },
+    {
+        "id": "17", "title": "Numerical Methods for Ordinary Differential Equations", "author": "Kees Vuik", "publication_year": 1978, "genre": "Essay",
+    },
+    {
+        "id": "18", "title": "A Practical Guide to Cloud Migration", "author": "Kieran Broadfoot", "publication_year": 1978, "genre": "Drama",
+    },
+    {
+        "id": "19", "title": "The European Experience", "author": "Jan Hansen, Jochen Hung,", "publication_year": 1978, "genre": "Essay",
+    },
+    {
+        "id": "20", "title": "HackSpace Magazine: Issue 63", "author": "HackSpace Team", "publication_year": 1978, "genre": "Short Story",
+    },
+    {
+        "id": "21", "title": "HackSpace magazine: Issue 64", "author": "HackSpace Team", "publication_year": 1990, "genre": "Comedy",
+    },
+    {
+        "id": "22", "title": "Introduction to Mechanical Engineering Design", "author": "Jacqulyn A. Baughman", "publication_year": 2013, "genre": "Novel",
+    },
+    {
+        "id": "23", "title": "Svelte Succinctly", "author": "Ed Freitas", "publication_year": 1980, "genre": "Comedy",
+    },
+    {
+        "id": "24", "title": "Introduction to Systems Biology", "author": "Thomas Sauter", "publication_year": 1976, "genre": "Fiction",
+    },
+    {
+        "id": "25", "title": "Blockchain For Dummies", "author": "Manav Gupta", "publication_year": 1992, "genre": "Fiction",
+    },
+    {
+        "id": "26", "title": "ASP.NET Core APIs Succinctly", "author": "Dirk Strauss", "publication_year": 1978, "genre": "Drama",
+    },
+    {
+        "id": "27", "title": "Numerical Methods for Ordinary Differential Equations", "author": "Kees Vuik", "publication_year": 1978, "genre": "Essay",
+    },
+    {
+        "id": "28", "title": "A Practical Guide to Cloud Migration", "author": "Kieran Broadfoot", "publication_year": 1978, "genre": "Drama",
+    },
+    {
+        "id": "29", "title": "The European Experience", "author": "Jan Hansen, Jochen Hung ", "publication_year": 1978, "genre": "Essay",
+    },
+    {
+        "id": "30", "title": "HackSpace Magazine: Issue 63", "author": "HackSpace Team", "publication_year": 1978, "genre": "Short Story",
+    }
+]
+export default bookslist
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,163 +1,69 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import bookslist from './Bookslist'
 function Dashboard() {
-    const bookslist=[
-        {
-            "title": "The Official Raspberry Pi Handbook 2023",         
-            "authors": "David Crookes, PJ Evans, Rosie Hattersley, Phil King, Nicola King, KG Orphanides, Nik Rawlinson, Mark Vanstone",
-            "publication_year": 1978,
-            "genre": "Comedy",
-        },
-        {
-            "title": "Pay for Play",            
-            "authors": "Larry Wayte",
-            "publication_year": 1965,
-            "genre": "Fiction",
-        },
-        {
-            "title": "Azure Bot Service Succinctly",
-            "authors": "Ed Freitas",
-            "publication_year": 1995,
-            "genre": "Thriller",
-        },
-        {
-            "title": "Introductory Algebra",
-            "authors": "Anne Gloag, Andrew Gloag, Melissa Kramer",
-            "publication_year": 1987,
-            "genre": "Comedy",
-        },
-        {
-           "title": "Managing Cloud Native Data on Kubernetes",
-            "authors": "Jeff Carpenter, Patrick McFadin",
-            "publication_year": 1979,
-            "genre": "Novel",
-        },
-        {
-            "id": "1912047446",
-            "title": "An Introduction to C &amp; GUI Programming",
-            "authors": "Simon Long",
-            "publication_year": 2005,
-            "genre": "Comedy",
-        },
-        {
-            "id": "1711470546",
-            "title": "Contemporary Mathematics",
-            "authors": "Donna Kirk",
-            "publication_year": 2008,
-            "genre": "Poetry",
-        },
-        {
-            "id": "1805110195",
-            "title": "The Last Man Who Knew Everything",
-            "authors": "Andrew Robinson",
-            "publication_year": 1990,
-            "genre": "Comedy",
-        },
-        {
-            "id": "5685527822",
-            "title": "HackSpace Magazine: Issue 65",
-            "authors": "HackSpace Team",
-            "publication_year": 2010,
-            "genre": "Thriller",
-        },
-        {
-            "id": "5685535457",
-            "title": "Intro to Social Media",
-            "authors": "Cheryl Lawson",
-            "publication_year": 2000,
-            "genre": "Comedy",
-        },
-        {
-            "id": "5679752518",
-            "title": "HackSpace magazine: Issue 64",
-            "authors": "HackSpace Team",
-            "publication_year": 1990,
-            "genre": "Comedy",
-        },
-        {
-            "id": "5679742652",
-            "title": "Introduction to Mechanical Engineering Design",
-            "authors": "Jacqulyn A. Baughman",
-            "publication_year": 2013,
-            "genre": "Novel",
-        },
-        {
-            "id": "1642002305",
-            "title": "Svelte Succinctly",
-            "authors": "Ed Freitas",
-            "publication_year": 1980,
-            "genre": "Comedy",
-        },
-        {
-            "id": "1800644124",
-            "title": "Introduction to Systems Biology",
-            "authors": "Thomas Sauter, Marco Albrecht",
-            "publication_year": 1976,
-            "genre": "Fiction",
-        },
-        {
-            "id": "111954601X",
-            "title": "Blockchain For Dummies",
-            "authors": "Manav Gupta",
-            "publication_year": 1992,
-            "genre": "Fiction",
-        },
-        {
-            "id": "1642002283",
-            "title": "ASP.NET Core APIs Succinctly",
-            "authors": "Dirk Strauss",
-            "publication_year": 1978,
-            "genre": "Drama",
-        },
-        {
-            "id": "9463666656",
-            "title": "Numerical Methods for Ordinary Differential Equations",
-            "authors": "Kees Vuik, Fred Vermolen, Martin van Gijzen",
-            "publication_year": 1978,
-            "genre": "Essay",
-        },
-        {
-            "id": "1492095176",
-            "title": "A Practical Guide to Cloud Migration",
-            "authors": "Kieran Broadfoot",
-            "publication_year": 1978,
-            "genre": "Drama",
-        },
-        {
-            "id": "1800648723",
-            "title": "The European Experience",
-            "authors": "Jan Hansen, Jochen Hung, Jaroslav Ira, Judit Klement, Sylvain Lesage, Juan Luis Simal, Andrew Tompkins",
-            "publication_year": 1978,
-            "genre": "Essay",
-        },
-        {
-            "id": "5676984863",
-            "title": "HackSpace Magazine: Issue 63",
-            "authors": "HackSpace Team",
-            "publication_year": 1978,
-            "genre": "Short Story",
+    const [books, setBooks]= useState(bookslist)  
+    const [order,setOrder] = useState("ASC")
+    const [search, setSearch]= useState("")
+    function sorting(param){        
+        if(order=="ASC"){           
+            if(param=="publication_year"){
+                var finaldata=[...bookslist].sort((book1, book2)=> book1[param]< book2[param]? -1:1)
+            }
+            else{
+            var finaldata=[...bookslist].sort((book1, book2)=> book1[param].toLowerCase()< book2[param].toLowerCase()? -1:1)
+            }
+            setBooks(finaldata)
+            setOrder("DSC")
+        }      
+        if(order=="DSC"){            
+            if(param=="publication_year"){
+                var finaldata=[...bookslist].sort((book1, book2)=> book1[param]> book2[param]? -1:1)
+            }
+            else{
+            var finaldata=[...bookslist].sort((book1, book2)=> book1[param].toLowerCase()> book2[param].toLowerCase()? -1:1)
+            }
+            setBooks(finaldata)
+            setOrder("ASC")
         }
-    ]
-
+        
+    }
+    function handlesearch(e){
+        setSearch(e.target.value)
+        const filteredlist=search?[...bookslist].filter(book=>book.title.toLowerCase().includes(search.toLowerCase())): bookslist
+        console.log(filteredlist);
+        setBooks(filteredlist)
+    }
     
   return (
-    <div>       
+    <div>    
+            <input type= "text" className='searching' placeholder='Search by Title' onChange={
+                // setSearch(e.target.value)
+                // // const bookdata= [...bookslist].contains()
+                // setBooks([...bookslist].filter(book=>search?book.title.includes(search):book))
+                handlesearch
+                }></input>   
             <table>
-                <tr>
-                    <th>Title</th>
-                    <th>Authors</th>
-                    <th>Publication Year</th>
-                    <th>Genre</th>
+                <thead>
+                <tr>                   
+                    <th onClick={()=> sorting("title")}>Title</th>
+                    <th onClick={()=> sorting("author")}>Author</th>
+                    <th onClick={()=> sorting("publication_year")}>Publication Year</th>
+                    <th onClick={()=> sorting("genre")}>Genre</th>
                 </tr>
-                {bookslist.map(book=>
-                <tr>
+                </thead>
+                <tbody>
+                {books.map(book=>
+               
+                <tr key={book.id}>                    
                     <td>{book.title}</td>
-                    <td>{book.authors}</td>
+                    <td>{book.author}</td>
                     <td>{book.publication_year}</td>
                     <td>{book.genre}</td>
 
                 </tr>
-)}
+                )}
+                </tbody>
+
             </table>
            
         
