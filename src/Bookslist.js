e7ba4b501c15edb8b71b6aef816e469ef027ce3d
Penugonda <sundara.k.penugonda@accenture.com>
2023-12-23 00:21:45 +0530

    implemented sorting and deletion of data

--- src/Bookslist.js
+++ src/Bookslist.js
@@ -18,76 +18,76 @@ const bookslist=[
         "id": 6, "title": "An Introduction to C &amp; GUI Programming", "author": "Simon Long", "publication_year": 2005, "genre": "Comedy",
     },
     {
-        "id": "7", "title": "Contemporary Mathematics", "author": "Donna Kirk", "publication_year": 2008, "genre": "Poetry",
+        "id": 7, "title": "Contemporary Mathematics", "author": "Donna Kirk", "publication_year": 2008, "genre": "Poetry",
     },
     {
-        "id": "8", "title": "The Last Man Who Knew Everything", "author": "Andrew Robinson", "publication_year": 1990, "genre": "Comedy",
+        "id": 8, "title": "The Last Man Who Knew Everything", "author": "Andrew Robinson", "publication_year": 1990, "genre": "Comedy",
     },
     {
-        "id": "9", "title": "HackSpace Magazine: Issue 65", "author": "HackSpace Team", "publication_year": 2010, "genre": "Thriller",
+        "id": 9, "title": "HackSpace Magazine: Issue 65", "author": "HackSpace Team", "publication_year": 2010, "genre": "Thriller",
     },
     {
-        "id": "10", "title": "Intro to Social Media", "author": "Cheryl Lawson", "publication_year": 2000, "genre": "Comedy",
+        "id":10, "title": "Intro to Social Media", "author": "Cheryl Lawson", "publication_year": 2000, "genre": "Comedy",
     },
     {
-        "id": "11", "title": "HackSpace magazine: Issue 64", "author": "HackSpace Team", "publication_year": 1990, "genre": "Comedy",
+        "id": 11, "title": "HackSpace magazine: Issue 64", "author": "HackSpace Team", "publication_year": 1990, "genre": "Comedy",
     },
     {
-        "id": "12", "title": "Introduction to Mechanical Engineering Design", "author": "Jacqulyn A. Baughman", "publication_year": 2013, "genre": "Novel",
+        "id": 12, "title": "Introduction to Mechanical Engineering Design", "author": "Jacqulyn A. Baughman", "publication_year": 2013, "genre": "Novel",
     },
     {
-        "id": "13", "title": "Svelte Succinctly", "author": "Ed Freitas", "publication_year": 1980, "genre": "Comedy",
+        "id": 13, "title": "Svelte Succinctly", "author": "Ed Freitas", "publication_year": 1980, "genre": "Comedy",
     },
     {
-        "id": "14", "title": "Introduction to Systems Biology", "author": "Thomas Sauter", "publication_year": 1976, "genre": "Fiction",
+        "id": 14, "title": "Introduction to Systems Biology", "author": "Thomas Sauter", "publication_year": 1976, "genre": "Fiction",
     },
     {
-        "id": "15", "title": "Blockchain For Dummies", "author": "Manav Gupta", "publication_year": 1992, "genre": "Fiction",
+        "id": 15, "title": "Blockchain For Dummies", "author": "Manav Gupta", "publication_year": 1992, "genre": "Fiction",
     },
     {
-        "id": "16", "title": "ASP.NET Core APIs Succinctly", "author": "Dirk Strauss", "publication_year": 1978, "genre": "Drama",
+        "id": 16, "title": "ASP.NET Core APIs Succinctly", "author": "Dirk Strauss", "publication_year": 1978, "genre": "Drama",
     },
     {
-        "id": "17", "title": "Numerical Methods for Ordinary Differential Equations", "author": "Kees Vuik", "publication_year": 1978, "genre": "Essay",
+        "id": 17, "title": "Numerical Methods for Ordinary Differential Equations", "author": "Kees Vuik", "publication_year": 1978, "genre": "Essay",
     },
     {
-        "id": "18", "title": "A Practical Guide to Cloud Migration", "author": "Kieran Broadfoot", "publication_year": 1978, "genre": "Drama",
+        "id": 18, "title": "A Practical Guide to Cloud Migration", "author": "Kieran Broadfoot", "publication_year": 1978, "genre": "Drama",
     },
     {
-        "id": "19", "title": "The European Experience", "author": "Jan Hansen, Jochen Hung,", "publication_year": 1978, "genre": "Essay",
+        "id": 19, "title": "The European Experience", "author": "Jan Hansen, Jochen Hung,", "publication_year": 1978, "genre": "Essay",
     },
     {
-        "id": "20", "title": "HackSpace Magazine: Issue 63", "author": "HackSpace Team", "publication_year": 1978, "genre": "Short Story",
+        "id": 20, "title": "HackSpace Magazine: Issue 63", "author": "HackSpace Team", "publication_year": 1978, "genre": "Short Story",
     },
     {
-        "id": "21", "title": "HackSpace magazine: Issue 64", "author": "HackSpace Team", "publication_year": 1990, "genre": "Comedy",
+        "id": 21, "title": "HackSpace magazine: Issue 64", "author": "HackSpace Team", "publication_year": 1990, "genre": "Comedy",
     },
     {
-        "id": "22", "title": "Introduction to Mechanical Engineering Design", "author": "Jacqulyn A. Baughman", "publication_year": 2013, "genre": "Novel",
+        "id": 22, "title": "Introduction to Mechanical Engineering Design", "author": "Jacqulyn A. Baughman", "publication_year": 2013, "genre": "Novel",
     },
     {
-        "id": "23", "title": "Svelte Succinctly", "author": "Ed Freitas", "publication_year": 1980, "genre": "Comedy",
+        "id": 23, "title": "Svelte Succinctly", "author": "Ed Freitas", "publication_year": 1980, "genre": "Comedy",
     },
     {
-        "id": "24", "title": "Introduction to Systems Biology", "author": "Thomas Sauter", "publication_year": 1976, "genre": "Fiction",
+        "id": 24, "title": "Introduction to Systems Biology", "author": "Thomas Sauter", "publication_year": 1976, "genre": "Fiction",
     },
     {
-        "id": "25", "title": "Blockchain For Dummies", "author": "Manav Gupta", "publication_year": 1992, "genre": "Fiction",
+        "id": 25, "title": "Blockchain For Dummies", "author": "Manav Gupta", "publication_year": 1992, "genre": "Fiction",
     },
     {
-        "id": "26", "title": "ASP.NET Core APIs Succinctly", "author": "Dirk Strauss", "publication_year": 1978, "genre": "Drama",
+        "id": 26, "title": "ASP.NET Core APIs Succinctly", "author": "Dirk Strauss", "publication_year": 1978, "genre": "Drama",
     },
     {
-        "id": "27", "title": "Numerical Methods for Ordinary Differential Equations", "author": "Kees Vuik", "publication_year": 1978, "genre": "Essay",
+        "id": 27, "title": "Numerical Methods for Ordinary Differential Equations", "author": "Kees Vuik", "publication_year": 1978, "genre": "Essay",
     },
     {
-        "id": "28", "title": "A Practical Guide to Cloud Migration", "author": "Kieran Broadfoot", "publication_year": 1978, "genre": "Drama",
+        "id": 28, "title": "A Practical Guide to Cloud Migration", "author": "Kieran Broadfoot", "publication_year": 1978, "genre": "Drama",
     },
     {
-        "id": "29", "title": "The European Experience", "author": "Jan Hansen, Jochen Hung ", "publication_year": 1978, "genre": "Essay",
+        "id": 29, "title": "The European Experience", "author": "Jan Hansen, Jochen Hung ", "publication_year": 1978, "genre": "Essay",
     },
     {
-        "id": "30", "title": "HackSpace Magazine: Issue 63", "author": "HackSpace Team", "publication_year": 1978, "genre": "Short Story",
+        "id": 30, "title": "HackSpace Magazine: Issue 63", "author": "HackSpace Team", "publication_year": 1978, "genre": "Short Story",
     }
 ]
 export default bookslist
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,23 +4,30 @@ function Dashboard() {
     const [books, setBooks]= useState(bookslist)  
     const [order,setOrder] = useState("ASC")
     const [search, setSearch]= useState("")
+    const [form, setForm]= useState(false)
+    const [bookdetails, setBookdetails] = useState({
+        title: "",
+        author: "",
+        pub_year:0 ,
+        genre: ""
+      })
     function sorting(param){        
         if(order=="ASC"){           
             if(param=="publication_year"){
-                var finaldata=[...bookslist].sort((book1, book2)=> book1[param]< book2[param]? -1:1)
+                var finaldata=[...books].sort((book1, book2)=> book1[param]< book2[param]? -1:1)
             }
             else{
-            var finaldata=[...bookslist].sort((book1, book2)=> book1[param].toLowerCase()< book2[param].toLowerCase()? -1:1)
+            var finaldata=[...books].sort((book1, book2)=> book1[param].toLowerCase()< book2[param].toLowerCase()? -1:1)
             }
             setBooks(finaldata)
             setOrder("DSC")
         }      
         if(order=="DSC"){            
             if(param=="publication_year"){
-                var finaldata=[...bookslist].sort((book1, book2)=> book1[param]> book2[param]? -1:1)
+                var finaldata=[...books].sort((book1, book2)=> book1[param]> book2[param]? -1:1)
             }
             else{
-            var finaldata=[...bookslist].sort((book1, book2)=> book1[param].toLowerCase()> book2[param].toLowerCase()? -1:1)
+            var finaldata=[...books].sort((book1, book2)=> book1[param].toLowerCase()> book2[param].toLowerCase()? -1:1)
             }
             setBooks(finaldata)
             setOrder("ASC")
@@ -33,18 +40,50 @@ function Dashboard() {
         console.log(filteredlist);
         setBooks(filteredlist)
     }
+    var dupbooklist=bookslist
+    function deletedata(index, id){
+        console.log(index)
+        console.log("id is", id)
+        var finallist=dupbooklist.filter(eachbook=>eachbook.id!==id )
+        dupbooklist=finallist
+        setBooks(finallist)
+    }
+    const handleChange = e => {
+        setBookdetails({
+          ...bookdetails,
+          [e.target.name]: e.target.value,
+        })
+      }
+    function addformdata(){      
+        
+        let obj={            
+                "id": bookslist.length+1, "title": bookdetails.title, "author": bookdetails.author, "publication_year": bookdetails.author, "genre": bookdetails.genre,
+            }
+        console.log(obj);
+        var bookslistafteradd=[...books, obj]
+        setBooks(bookslistafteradd)
+    }
+    
     
   return (
     <div>    
-            <input type= "text" className='searching' placeholder='Search by Title' onChange={
-                // setSearch(e.target.value)
-                // // const bookdata= [...bookslist].contains()
-                // setBooks([...bookslist].filter(book=>search?book.title.includes(search):book))
-                handlesearch
-                }></input>   
+            <input type= "text" className='searching' placeholder='Search by Title' onChange={handlesearch}></input>   
+            <button onClick={()=>setForm(true)}>Add book to the below list</button>
+
+            {
+                form?<div>
+                    <input type='text' placeholder='Enter the title of book'name='title' value={bookdetails.title} onChange={handleChange}></input>
+                    <input type='text' placeholder='Enter the Author of book'name='author' value={bookdetails.author} onChange={handleChange}></input>
+                    <input type='number' placeholder='Enter the Publication Year' name='pub_year' value={bookdetails.pub_year} onChange={handleChange}></input>
+                    <input type='text' placeholder='Enter the Genre of book' name='genre' value={bookdetails.genre}  onChange={handleChange}></input>
+                    <button onClick={addformdata}>Submit</button>
+                </div>:
+                <div></div>
+            }
             <table>
                 <thead>
-                <tr>                   
+                <tr>   
+                    <th>Id</th>                
                     <th onClick={()=> sorting("title")}>Title</th>
                     <th onClick={()=> sorting("author")}>Author</th>
                     <th onClick={()=> sorting("publication_year")}>Publication Year</th>
@@ -52,15 +91,17 @@ function Dashboard() {
                 </tr>
                 </thead>
                 <tbody>
-                {books.map(book=>
-               
-                <tr key={book.id}>                    
+                {books.map((book, index)=>
+                
+                <tr key={index}> 
+                    <td>{book.id}</td>                   
                     <td>{book.title}</td>
                     <td>{book.author}</td>
                     <td>{book.publication_year}</td>
                     <td>{book.genre}</td>
-
+                    <td><button onClick={()=>{ deletedata(index, book.id)}}>Delete</button></td>
                 </tr>
+                
                 )}
                 </tbody>
 
